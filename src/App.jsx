ed71542c01b3a5ece616eb7f478f8211b6c3b950
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-04-26 08:32:51 -0300

    apiFunction função
    Função criada com o objetivo de amenizar a alta acoplagem do código anterior.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import Header from './Components/Header';
 import BodyNList from './Components/BodyNList';
 import InputButton from './Components/InputButton';
-import { InputValueProvider, useInputValue } from './Components/Context';
+import { InputValueProvider } from './Components/Context';
 import DivLoading from './Components/DivLoading';
 
 function App() {
@@ -14,6 +14,37 @@ function App() {
     console.log(searchResult);
   }, [searchResult]);
 
+  const apiFunction = (inputValue) => {
+    fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${inputValue}`)
+    .then((response) => {
+      if (response.status === 200) {
+        setCarregando(false);
+        return response.json();
+      } else {
+        setCarregando(false);
+        throw new Error(
+          'Digite um número entre 1 e 389 ou um nome de uma entidade existente, por gentileza amigão!',
+        );
+      }
+    })
+    .then((data) => {
+      {
+        searchResult.map((searched) => {
+          if (searched.name === data.data.name) {
+            throw new Error(
+              'Entidade ja pesquisada mano! Dá uma olhada aí que cê acha ela!',
+            );
+          }
+        });
+      }
+      setSearchResult((result) => [...result, data.data]);
+    })
+    .catch((error) => {
+      setErrorMessage(`${error.message}`);
+      console.log(error);
+    });
+  }
+
   const handleSearch = useMemo(() => {
     return (inputValue) => {
       if (inputValue.trim() === '') {
@@ -23,36 +54,7 @@ function App() {
       } else {
         setErrorMessage('');
         setCarregando(true);
-        fetch(
-          `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${inputValue}`,
-        )
-          .then((response) => {
-            if (response.status === 200) {
-              setCarregando(false);
-              return response.json();
-            } else {
-              setCarregando(false);
-              throw new Error(
-                'Digite um número entre 1 e 389 ou um nome de uma entidade existente, por gentileza amigão!',
-              );
-            }
-          })
-          .then((data) => {
-            {
-              searchResult.map((searched) => {
-                if (searched.name === data.data.name) {
-                  throw new Error(
-                    'Entidade ja pesquisada mano! Dá uma olhada aí que cê acha ela!',
-                  );
-                }
-              });
-            }
-            setSearchResult((result) => [...result, data.data]);
-          })
-          .catch((error) => {
-            setErrorMessage(`${error.message}`);
-            console.log(error);
-          });
+        apiFunction(inputValue);
       }
     };
   });
