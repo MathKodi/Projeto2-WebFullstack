44294f9da89f91786f69493d911a7e2c6c1d8ec7
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-08-18 12:41:43 -0300

    Esqueleto página de Login
    Adicionado form, com função de handle para o formulário

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const LoginPage = () => {
+
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLogin("");
+    setSenha("");
+  }
+
   return (
     <div>
-        <h1>Página de login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label> 
+              <span>Login: </span>
+              <input type='text' name='login' placeholder='Digite seu login aqui' onChange={(e) => setLogin(e.target.value)}></input>
+            </label>
+          </div>
+          <label>
+            <span>Senha: </span>
+            <input type='password' name='senha' placeholder='Digite sua senha aqui'  onChange={(e) => setSenha(e.target.value)} />
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
     </div>
   )
 }
